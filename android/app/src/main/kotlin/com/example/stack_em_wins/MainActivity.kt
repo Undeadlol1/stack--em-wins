65d00254aec659ba8022b9987d29299b23cf13f7
Mihail Paley <undeadlol1@mail.ru>
2020-03-01 20:47:04 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/stack_em_wins/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stack_em_wins
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
